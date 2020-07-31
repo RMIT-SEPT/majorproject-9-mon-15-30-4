2f0761146d52b1d93f019eaac6d509d1e1829565
HillsonNgo <61672571+ngoHill@users.noreply.github.com>
2020-07-31 11:20:28 +1000

    Initial Commit of core files
    Created backend project of springboot as per requirements of task/tutorial. Added changes to particular application.properties as per tutorial.

---
+++ BackEnd/springboot-backend/src/main/java/com/sept/majorproject/group09/mon/sbbackend/SbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sept.majorproject.group09.mon.sbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbBackendApplication.class, args);
+    }
+
+}
