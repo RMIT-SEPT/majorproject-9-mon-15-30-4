060a746cf591f05b1857696413b94cbc902d4c9e
AlexanderMurphy3657274 <s3657274@student.rmit.edu.au>
2020-09-16 21:41:42 +1000

    Working hours overlap compensation logic added to backend. (Completely untested)

--- BackEnd/springboot-backend/src/main/java/com/sept/majorproject/group09/mon/sbbackend/services/WorkingHoursService.java
+++ BackEnd/springboot-backend/src/main/java/com/sept/majorproject/group09/mon/sbbackend/services/WorkingHoursService.java
@@ -2,10 +2,12 @@ package com.sept.majorproject.group09.mon.sbbackend.services;
 
 import com.sept.majorproject.group09.mon.sbbackend.model.WorkingHours;
 import com.sept.majorproject.group09.mon.sbbackend.repositories.WorkingHoursRepository;
+import org.hibernate.jdbc.Work;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,7 +29,101 @@ public class WorkingHoursService {
     }
 
     public void saveOrUpdate(WorkingHours workingHours) {
-        workingHoursRepository.save(workingHours);
+        List<WorkingHours> workingHoursList = workingHoursRepository.findAllByEmployee(workingHours.getEmployeeId()),
+            overlap = new ArrayList<>();
+        Calendar existing = Calendar.getInstance(), newHours = Calendar.getInstance();
+        newHours.setTime(workingHours.getDate());
+
+        for (WorkingHours hours : workingHoursList) {
+            existing.setTime(hours.getDate());
+            // If on same date
+            if(newHours.get(Calendar.YEAR) == existing.get(Calendar.YEAR)
+                    && newHours.get(Calendar.MONTH) == existing.get(Calendar.MONTH)
+                    && newHours.get(Calendar.DAY_OF_MONTH) == existing.get(Calendar.DAY_OF_MONTH)){
+
+                if(workingHours.getStartTime() >= hours.getStartTime()
+                        && workingHours.getStartTime() <= hours.getEndTime()) // Starts overlap
+                    overlap.add(hours);
+                else if(workingHours.getEndTime() >= hours.getStartTime()
+                        && workingHours.getEndTime() <= hours.getEndTime()) // Ends overlap
+                    overlap.add(hours);
+            }
+        }
+
+        Calendar earliestDate = Calendar.getInstance(), latestDate = Calendar.getInstance();
+        earliestDate.setTime(workingHours.getDate());
+        latestDate.setTime(workingHours.getDate());
+        double earliestTime = workingHours.getStartTime(), latestTime = workingHours.getEndTime();
+
+        for (WorkingHours hours : overlap) {
+            workingHoursRepository.deleteById(hours.getId());
+
+            existing.setTime(hours.getDate());
+            if(existing.get(Calendar.YEAR) < earliestDate.get(Calendar.YEAR)) {
+                earliestDate.setTime(hours.getDate());
+                earliestTime = hours.getStartTime();
+                continue;
+            } else if(existing.get(Calendar.YEAR) > earliestDate.get(Calendar.YEAR)) {
+                latestDate.setTime(hours.getDate());
+                latestTime = hours.getEndTime();
+                continue;
+            } else {
+                if(existing.get(Calendar.MONTH) < earliestDate.get(Calendar.MONTH)) {
+                    earliestDate.setTime(hours.getDate());
+                    earliestTime = hours.getStartTime();
+                    continue;
+                } else if(existing.get(Calendar.MONTH) > earliestDate.get(Calendar.MONTH)) {
+                    latestDate.setTime(hours.getDate());
+                    latestTime = hours.getEndTime();
+                    continue;
+                } else {
+                    if(existing.get(Calendar.DAY_OF_MONTH) < earliestDate.get(Calendar.DAY_OF_MONTH)) {
+                        earliestDate.setTime(hours.getDate());
+                        earliestTime = hours.getStartTime();
+                        continue;
+                    } else if(existing.get(Calendar.DAY_OF_MONTH) > earliestDate.get(Calendar.DAY_OF_MONTH)) {
+                        latestDate.setTime(hours.getDate());
+                        latestTime = hours.getEndTime();
+                        continue;
+                    } else {
+                        if(hours.getStartTime() < earliestTime) {
+                            earliestDate.setTime(hours.getDate());
+                            earliestTime = hours.getStartTime();
+                            continue;
+                        } else if(hours.getEndTime() < latestTime) {
+                            latestDate.setTime(hours.getDate());
+                            latestTime = hours.getEndTime();
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+
+        if(earliestDate == latestDate) {
+            workingHours.setDate(earliestDate.getTime());
+            workingHours.setStartTime(earliestTime);
+            workingHours.setEndTime(latestTime);
+            workingHoursRepository.save(workingHours);
+        } else {
+            WorkingHours insertHours;
+            newHours.setTime(earliestDate.getTime());
+            insertHours = new WorkingHours(workingHours.getEmployeeId(), earliestTime, 24.0, newHours.getTime(),
+                    workingHours.getDay());
+            workingHoursRepository.save(insertHours);
+            newHours.add(Calendar.DATE, 1);
+            while(newHours.get(Calendar.YEAR) != latestDate.get(Calendar.YEAR)
+                    && newHours.get(Calendar.MONTH) != latestDate.get(Calendar.MONTH)
+                    && newHours.get(Calendar.DAY_OF_MONTH) != latestDate.get(Calendar.DAY_OF_MONTH)){
+                insertHours = new WorkingHours(workingHours.getEmployeeId(), 0, 24.0, newHours.getTime(),
+                workingHours.getDay());
+                workingHoursRepository.save(insertHours);
+            }
+            newHours.setTime(latestDate.getTime());
+            insertHours = new WorkingHours(workingHours.getEmployeeId(), 0, latestTime, newHours.getTime(),
+                    workingHours.getDay());
+            workingHoursRepository.save(insertHours);
+        }
     }
 
     public Optional<WorkingHours> findById(long id){
