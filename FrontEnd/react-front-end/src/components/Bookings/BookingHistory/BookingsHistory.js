0581347487c0f8176b4b5bf5f0b7c3229e1b5cb5
s3601219 <s3601219@student.rmit.edu.au>
2020-09-20 21:59:51 +1000

    Booking History table added

---
+++ FrontEnd/react-front-end/src/components/Bookings/BookingHistory/BookingsHistory.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import * as ReactBootStrap from "react-bootstrap";
+import './BookingsHistoryCSS.css'
+
+class BookingsHistory extends React.Component {
+    
+    constructor(){
+        super();
+        this.state = {
+            bookings: []
+        };
+    }
+
+    async componentDidMount(){
+        const url = "http://localhost:8080/api/bookings/all";
+        const response = await fetch(url);
+        let data = await response.json();
+        data = Array.isArray(data) ? data : [data];
+        console.dir(data);
+
+        this.setState({
+            bookings: data.map(this.renderBookings)
+        })
+    }
+   
+    renderBookings(booking, index) {
+        return(
+            <tr key={index}>
+              <td>{booking.id}</td>
+              <td>{booking.date}</td>
+              <td>{booking.customerId}</td>
+              <td>{booking.employeeId}</td>
+              <td>{booking.serviceId}</td>              
+            </tr>
+        )
+    }
+
+    render() {
+        return(
+    <div>
+    <ReactBootStrap.Table striped bordered hover size="sm" id="table1">
+        <thead>
+            <tr>
+            <th>ID</th>
+            <th>Date</th>
+            <th>CustomerID</th>
+            <th>EmployeeID</th>
+            <th>ServiceID</th>
+            </tr>
+        </thead>
+    <tbody>
+        {this.state.bookings} 
+    </tbody>
+    </ReactBootStrap.Table>
+    </div>
+        )
+    }
+}
+
+export default BookingsHistory;
